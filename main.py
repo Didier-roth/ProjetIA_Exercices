498fb64f59cb3d039deb267cb5ecf35dc7541bb9
didier <didier.roth@student.hepl.be>
2024-09-19 10:18:05 +0200

    :construction: test with another value

--- main.py
+++ main.py
@@ -34,15 +34,17 @@ class Nnet:
 w = np.array([0,0])
 th = 0
 res = Nnet(w,th)
-S = np.array([[0,0],[0,1],[1,0],[1,1]])
-T = np.array([[0],[1],[1],[1]])
+S = np.array([[2,5],[3,5],[4,4],[3,3],[2,2],[4,2],[3.88,7.3],[4.7,6.58],[5.88,5.48],[7,6],[6.8,7.34],[5.62,8.22]])
+T = np.array([[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1]])
 
-res.train(S,T,10)
+res.train(S,T,100)
 res.eval(S)
 print(res.w)
 
-# absc = np.arange(-1,2.1,0.1)
-# ord = (th-absc*w[0])/w[1]
-# plt.scatter(x[:,0],x[:,1])
-# plt.plot(absc,ord)
-# plt.show()
+absc = np.arange(1,7.5,0.1)
+ord = (res.w[2]-absc*res.w[0])/res.w[1]
+plt.scatter(S[:,0],S[:,1])
+plt.plot(absc,ord)
+plt.ylim(top=10, bottom = 0)
+plt.xlim(-4,10)
+plt.show()
